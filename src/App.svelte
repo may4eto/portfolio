5331067bbd7c8b71a0397c2391611da6c170bd55
MayaMircheva <may4eto@hotmail.com>
2024-10-02 22:02:00 +0200

    improvements

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script>
+
+
   import { onMount } from "svelte";
   import { data } from './data.js'
   import ProfileCard from './lib/ProfileCard.svelte'
@@ -9,20 +11,18 @@
   import ProjectsCard from './lib/ProjectsCard.svelte';
   import ProjectGrid from './lib/ProjectGrid.svelte';
   import Footer from './lib/Footer.svelte'
+  import P5 from 'p5-svelte';
   let blogData = []
   let filterTerm = ""
   let filteredProjects = []
   let selected= ""
-  onMount(async () => {
-        const response = await fetch(
-                'https://dev.to/api/articles?username=may4eto',
-                {
-                    method: 'GET',
-                }
-            );
-            const data = await response.json();
-            blogData = data[0]
-  });
+  const fetchData = (async () => {
+    const response = await fetch('https://dev.to/api/articles?username=may4eto', {method: "GET"})
+    const data = await response.json()
+    blogData = data[0]
+
+  })
+  onMount(fetchData);
   //filter function
   const filterProjects = (event) => {
     filterTerm = `${event.detail.text.trim()}`
@@ -34,28 +34,58 @@
     filteredProjects = []
     selected = ""
   }
+  //background
+  let t
+  let nl
+  let canvasHeight
+  let canvasWidth
+  let totalX
+  let totalY
+  let density
+
+  const htmlTag = document.querySelector("html")
+
+  const sketch = (p5) => {
+  p5.setup = () => {
+    nl = 0.01
+    density = 5
+    canvasHeight = htmlTag.scrollHeight
+    canvasWidth = htmlTag.clientWidth
+    totalX = canvasWidth
+    totalY = canvasHeight
+    let c1 = p5.color("#c4c4c4")
+    let c2 = p5.color("#828282")
+    p5.createCanvas(canvasWidth, canvasHeight)
+    for(let x = 0; x < totalX; x = x + 1 ) {
+      for(let y = 0; y < totalY; y = y + 1) {
+        let n = p5.noise(x * nl, y * nl)
+        let rn = n * density - p5.floor(n * density)
+        let c = p5.lerpColor(c1, c2, rn)
+        p5.set(x, y, c)
+      }
+    }
+    p5.updatePixels()
+  }
+  }
 </script>
 
-{#if blogData}
-    <main>
-      <ProfileCard {data} />
-      <section class="intro">
-        <SkillsCard {data} />
-        <BlogCard {blogData} />
-        <HobbiesCard {data} />
-        <ExperienceCard {data} />
-      </section>
-      <section class="projects">
-        <ProjectsCard {data} {filteredProjects} {selected} on:filter={filterProjects} on:clear={clearFilters}/>
-        {#key filteredProjects}
-          <ProjectGrid {data} {filteredProjects} />
-        {/key}
-      </section>
-    </main>
-    <Footer />
-{:else}
-  ...loading
-{/if}
+<main>
+  <ProfileCard {data} />
+  <section class="intro">
+    <SkillsCard {data} />
+    <BlogCard {blogData}/>
+    <HobbiesCard {data} />
+    <ExperienceCard {data} />
+  </section>
+  <section class="projects">
+    <ProjectsCard {data} {filteredProjects} {selected} on:filter={filterProjects} on:clear={clearFilters}/>
+    {#key filteredProjects}
+      <ProjectGrid {data} {filteredProjects} />
+    {/key}
+  </section>
+</main>
+<Footer />
+<P5 {sketch} />
 
 <style>
   @import '../src/assets/global.css';
--- src/lib/BlogCard.svelte
+++ src/lib/BlogCard.svelte
@@ -1,19 +1,27 @@
 <script>
 // @ts-nocheck
-export let blogData 
+export let blogData
+import { Circle } from 'svelte-loading-spinners';
 </script>
 
 <div class="card">
     <h4>Blog</h4>
+    {#if Object.keys(blogData).length === 0}
+    <div class="spinner"><Circle size="60" color="#4f4f4f" unit="px" /></div>
+    {:else} 
     <div class="header">
         <img src="{blogData.cover_image}" alt="" />
         <h3>{blogData.title}</h3>
     </div>
     <p>{blogData.description}</p>
     <a href="{blogData.url}" alt="" target="_blank">dev.to</a>
+    {/if}
 </div>
 
 <style>
+    .card {
+        height: 520px;
+    }
     .card h3 {
         font-size: 24px;
         margin-top: 8px;
@@ -31,5 +39,12 @@ export let blogData
     .card .header img {
         width: 100%;
         border-radius: 12px;
+    }
+    .spinner {
+        height: 100%;
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }
 </style>
